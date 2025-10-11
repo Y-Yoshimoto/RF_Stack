5ca409c2bc37695c64202d3ce276e2e6f926aeee
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-10-11 21:55:58 +0900

    refacotr: 命名規則の修正

--- fastapi_app/src/app.py
+++ fastapi_app/src/app.py
@@ -4,14 +4,14 @@ import uvicorn
 from fastapi import FastAPI
 
 # サブモジュール読み込み
-from RestSample.apiapp import router as RestSample
-from Proxy.apiapp import router as Proxy
+from rest_sample.apiapp import router as RestSample
+from proxy.apiapp import router as proxy
 
 # アプリケーション起動
 app = FastAPI()
 # サブモジュール読み込み
 app.include_router(RestSample)
-app.include_router(Proxy)
+app.include_router(proxy)
 
 
 @app.get("/", include_in_schema=False)
