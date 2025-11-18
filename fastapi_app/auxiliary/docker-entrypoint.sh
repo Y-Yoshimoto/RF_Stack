6611b47884eba837d320f3f142bba499af817008
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-11-18 23:26:42 +0900

    fix: 仮想環境の有効化

--- fastapi_app/auxiliary/docker-entrypoint.sh
+++ fastapi_app/auxiliary/docker-entrypoint.sh
@@ -10,7 +10,7 @@ uv sync --locked 2>&1 || uv sync 2>&1 | tee -a ${UVINSTALLLOCK}
 # echo "Installing Playwright browsers. ---------------------" | tee -a ${UVINSTALLLOCK}
 # uv run playwright install chromium | tee -a ${UVINSTALLLOCK}
 # echo "Installation completed." | tee -a ${UVINSTALLLOCK}
-# uv 仮想環境の有効化
+# uv 仮想環境の有効化(source .venv/bin/activate)
 . .venv/bin/activate 2>&1 | tee -a ${UVINSTALLLOCK}
 rm ${UVINSTALLLOCK}
 echo "End Setup."
