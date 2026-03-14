a6d468544a28cb255f1e92c6367db5a4a9783554
Y-Yoshimoto <mewyoshimoto@gmail.com>
2026-03-14 16:34:52 +0900

    add: StoryBookテンプレート

--- react_app/eslint.config.js
+++ react_app/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -6,36 +9,33 @@ import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from "eslint-config-prettier";
 
 
-export default tseslint.config(
-  { ignores: ['dist', 'playwright-report'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      // React Hooks用の推奨ルールを適用
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      // 環境変数使用の警告をオフ
-      'no-process-env': ['off'],
-      // 'react-refresh/only-export-components'の警告をオフ
-      'react-refresh/only-export-components': 'off',
-      // _や__で始まる変数名、_mockで始まる変数名を無視
-      '@typescript-eslint/no-unused-vars': ['warn', { 'varsIgnorePattern': '^(_|__.*|_mock.*)$' }],
-      // 循環的複雑度の警告
-      // "complexity": ["warn", 0],
-      "complexity": ["warn", 15],
-      // ファイルの最大行数を500に設定
-      "max-lines": ["warn", 500],
-      // React Hooksの依存関係配列の警告をオフ
-      "react-hooks/exhaustive-deps": ['off'],
-    },
+export default tseslint.config({ ignores: ['dist', 'playwright-report'] }, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+  files: ['**/*.{ts,tsx,js,jsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-)
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    // React Hooks用の推奨ルールを適用
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // 環境変数使用の警告をオフ
+    'no-process-env': ['off'],
+    // 'react-refresh/only-export-components'の警告をオフ
+    'react-refresh/only-export-components': 'off',
+    // _や__で始まる変数名、_mockで始まる変数名を無視
+    '@typescript-eslint/no-unused-vars': ['warn', { 'varsIgnorePattern': '^(_|__.*|_mock.*)$' }],
+    // 循環的複雑度の警告
+    // "complexity": ["warn", 0],
+    "complexity": ["warn", 15],
+    // ファイルの最大行数を500に設定
+    "max-lines": ["warn", 500],
+    // React Hooksの依存関係配列の警告をオフ
+    "react-hooks/exhaustive-deps": ['off'],
+  },
+}, storybook.configs["flat/recommended"]);
--- react_app/vitest.config.js
+++ react_app/vitest.config.js
@@ -5,47 +5,76 @@ import { defineConfig } from "vitest/config";
 process.env.TZ = "Asia/Tokyo";
 // テストの実行環境の言語を指定
 process.env.LANG = "ja_JP.UTF-8";
-
 import * as path from "path";
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        // reporters: ['json', 'default'],
-        reporters: ['default'],
-        globals: true,
-        environment: "jsdom",
-        printConsoleTrace: true,
-        include: ["./src/**/*.{test,spec}.{js,ts,jsx,tsx}", "'./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'"],
-        exclude: ["node_modules", "dist", "build"],
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-        // https://vitest.dev/guide/coverage.html#coverage
-        coverage: {
-            provider: 'v8',
-            include: ['src/**/*.{js,ts,jsx,tsx}'],
-            exclude: [
-                // テストコードは除外
-                'src/**/*.{test,spec}.{js,ts,jsx,tsx}',
-                // アプリケーション全体に関わるコンポーネントは除外
-                'src/main.tsx', 'src/App.tsx', 'src/theme.tsx', 'src/serviceworker.js',
-                // ルーティングに関わるファイルは除外
-                'src/routes/**/*.{js,ts,jsx,tsx}'
-            ],
-            reporter: ['text', 'json', 'html', 'lcov'],
-            // reporter: ['html'],
-        },
-        // https://vitest.dev/guide/browser/
-        // https://vitest.dev/guide/browser/config.html
-        browser: {
-            provider: 'playwright',
-            enabled: true,
-            headless: true,
-            viewport: { width: 1280, height: 720 }, // default { width: 414, height: 896 }
-            screenshotFailures: false,
-            instances: [
-                { browser: 'chromium' },
-            ],
-        },
+  plugins: [react()],
+  test: {
+    // reporters: ['json', 'default'],
+    reporters: ['default'],
+    globals: true,
+    environment: "jsdom",
+    printConsoleTrace: true,
+    include: ["./src/**/*.{test,spec}.{js,ts,jsx,tsx}", "'./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'"],
+    exclude: ["node_modules", "dist", "build"],
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     },
+    // https://vitest.dev/guide/coverage.html#coverage
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{js,ts,jsx,tsx}'],
+      exclude: [
+        // テストコードは除外
+        'src/**/*.{test,spec}.{js,ts,jsx,tsx}',
+        // アプリケーション全体に関わるコンポーネントは除外
+        'src/main.tsx', 'src/App.tsx', 'src/theme.tsx', 'src/serviceworker.js',
+        // ルーティングに関わるファイルは除外
+        'src/routes/**/*.{js,ts,jsx,tsx}'],
+      reporter: ['text', 'json', 'html', 'lcov']
+      // reporter: ['html'],
+    },
+    // https://vitest.dev/guide/browser/
+    // https://vitest.dev/guide/browser/config.html
+    browser: {
+      provider: playwright(),
+      enabled: true,
+      headless: true,
+      viewport: {
+        width: 1280,
+        height: 720
+      },
+      // default { width: 414, height: 896 }
+      screenshotFailures: false,
+      instances: [{
+        browser: 'chromium'
+      }]
+    },
+    // projects: [{
+    //   extends: true,
+    //   plugins: [
+    //     // The plugin will run tests for the stories defined in your Storybook config
+    //     // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+    //     storybookTest({
+    //       configDir: path.join(dirname, '.storybook')
+    //     })],
+    //   test: {
+    //     name: 'storybook',
+    //     browser: {
+    //       enabled: true,
+    //       headless: true,
+    //       provider: playwright({}),
+    //       instances: [{
+    //         browser: 'chromium'
+    //       }]
+    //     },
+    //     setupFiles: ['.storybook/vitest.setup.ts']
+    //   }
+    // }]
+  }
 });
